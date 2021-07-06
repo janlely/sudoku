aa6e4f8ee430f0d7bb0b5dcf0df0f1c045df2d3a
金俊杰 <janlely@163.com>
2021-07-06 21:54:23 +0800

    add input

--- bfsearch/app/Main.hs
+++ bfsearch/app/Main.hs
@@ -1,24 +1,24 @@
 module Main where
 
 import Lib
-import Graphics.Gloss
-
-window :: Display
-window = InWindow "Nice Window" (200, 200) (10, 10)
-
-background :: Color
-background = white
-
-gridPoints :: Path
-gridPoints = [(10 + x * 20, 10 + y * 20) | x <- [0..9], [0..9]]
-
-drawing :: Picture
-drawing = polygon gridPoints
-
-main :: IO ()
-main = display window background drawing
-
+-- import Graphics.Gloss
+--
+-- window :: Display
+-- window = InWindow "Nice Window" (200, 200) (10, 10)
+--
+-- background :: Color
+-- background = white
+--
+-- gridPoints :: Path
+-- gridPoints = [(10 + x * 20, 10 + y * 20) | x <- [0..9], [0..9]]
+--
+-- drawing :: Picture
+-- drawing = polygon gridPoints
+--
 -- main :: IO ()
--- main = solveSudoku
+-- main = display window background drawing
+--
+main :: IO ()
+main = solveSudoku
 
 
